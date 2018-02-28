feac4d6d7140f59714e2cb69c296ab92990b9489
JayLatt <jlattimor3@gmail.com>
2018-02-28 13:40:27 -0500

    moved config data

--- config/application.rb
+++ config/application.rb
@@ -26,13 +26,14 @@ module Orgrel
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
 
-#API privileges
-config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #API privileges
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
   end
 end
