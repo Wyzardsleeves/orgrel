40786f4da158e6d798523a912d39cc5a39aab2d0
JayLatt <jlattimor3@gmail.com>
2018-02-28 01:42:29 -0500

    altered gemfile for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'home#index'
+  get "home/index"
+  get 'home/about'
+
   namespace 'api' do
     namespace 'v1' do
       resources :users
