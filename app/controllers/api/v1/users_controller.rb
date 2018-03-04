4722c70edd31b5c710f4346b8c503dc5c99db134
JayLatt <jlattimor3@gmail.com>
2018-03-03 20:33:21 -0500

    laisejf

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      skip_before_action :verify_authenticity_token
       
       def index
         users = User.order('created_at ASC')
